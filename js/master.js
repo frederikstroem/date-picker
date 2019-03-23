70506f842d0c07bd69ffbd94c5fe6fbd6a869208
Katofln <mail@frederikstroem.com>
2019-03-23 11:24:34 +0100

    Added source to function

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 /**
  * Make a prompt to make text copyable.
  * @param {string} text - Text to clipboard.
+ * Source: https://stackoverflow.com/a/6055620
 */
 function copyToClipboard(text) {
   window.prompt("Copy to clipboard", text);
